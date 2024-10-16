9b26a45dca4368a1926d7d75b42a9a67709dfffc
Chrispat616 <chrispat616@gmail.com>
2024-10-17 00:31:49 +0300

    Added Like/Unlike functionality

--- src/Components/Feedposts/PostFooter.jsx
+++ src/Components/Feedposts/PostFooter.jsx
@@ -1,79 +1,112 @@
-import { Flex, Box, Text, InputRightElement, InputGroup, Button, Input} from "@chakra-ui/react";
-import { useState } from "react";
-import {CommentLogo, NotificationsLogo, UnlikeLogo, MessagesLogo, BookMarkLogo} from '../../assets/constants'
+import {
+  Flex,
+  Box,
+  Text,
+  InputRightElement,
+  InputGroup,
+  Button,
+  Input,
+} from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import {
+  CommentLogo,
+  NotificationsLogo,
+  UnlikeLogo,
+  MessagesLogo,
+  BookMarkLogo,
+} from "../../assets/constants";
+import usePostComment from "../../hooks/usePostComment";
+import useAuthStore from "../../store/authStore";
+import useLikePosts from "../../hooks/useLikePosts";
 
-const PostFooter = ({username, isProfilePage}) => {
-  const[liked,setLiked]= useState(false);
-  const[likes, setLikes] =useState(2100);
+const PostFooter = ({ post, username, isProfilePage }) => {
+  const { isCommenting, handlePostComment } = usePostComment();
   const [comment, setComment] = useState("");
+  const authUser = useAuthStore((state) => state.user);
+  const commentRef = useRef(null);
+  const { handleLikePost, isLiked, likes } = useLikePosts(post);
 
-  const handleLike = () =>{
-    if(liked){
-      setLiked(false);
-      setLikes(likes -1);
-    }else {
-      setLiked(true);
-      setLikes(likes +1);
-    }
-  }
-  const handleCommentChange = (e) => {
-    setComment(e.target.value); 
+  const handleSubmitComment = async () => {
+    await handlePostComment(post.id, comment);
+    setComment("");
   };
+
   return (
     <>
-    <Box mb={8} marginTop={"auto"}>
-      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mt={"2"}>
-        <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
-          {!liked ? (<NotificationsLogo/>): (<UnlikeLogo/>)}
-        </Box>
-        <Box cursor={"pointer"} fontSize={18}>
-          <CommentLogo/>
-        </Box>
-        <Box cursor={"pointer"} fontSize={18}>
-          <MessagesLogo/>
-        </Box>
-        <Box  cursor={"pointer"}  ml={"auto"} >
-            <BookMarkLogo />  
-        </Box>
-      </Flex>
-      <Text fontWeight={600} fontSize={"sm"}> 
-        {likes} likes
-      </Text>
-     {!isProfilePage && (
-      <>
-         <Text fontSize={"sm"} fontWeight={700}>
-        {username}{"   "}
-        <Text as='span'  fontWeight={400}>
-         We just getting started 
+      <Box mb={8} marginTop={"auto"}>
+        <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mt={"2"}>
+          <Box onClick={handleLikePost} cursor={"pointer"} fontSize={18}>
+            {!isLiked ? <NotificationsLogo /> : <UnlikeLogo />}
+          </Box>
+          <Box
+            cursor={"pointer"}
+            fontSize={18}
+            onClick={() => commentRef.current.focus()}
+          >
+            <CommentLogo />
+          </Box>
+          <Box cursor={"pointer"} fontSize={18}>
+            <MessagesLogo />
+          </Box>
+          <Box cursor={"pointer"} ml={"auto"}>
+            <BookMarkLogo />
+          </Box>
+        </Flex>
+        <Text fontWeight={600} fontSize={"sm"}>
+          {likes} likes
         </Text>
-      </Text>
-      <Text fontSize={"small"} color={"grey"} cursor={"pointer"} >
-        View all 1,500 comments
-      </Text>
-      </>)}
-      <Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"}>
-      <InputGroup >
-      <Input variant={"flushed"} placeholder={"Add a comment..."} fontSize={14}
-      value={comment} 
-      onChange={handleCommentChange} 
-      />
-       <InputRightElement>
-       {comment && ( // Conditionally render button based on comment state
-         <Button  fontSize={14} 
-         color={"grey"} 
-         fontWeight={600} 
-         cursor={"pointer"} 
-         _hidden={true} 
-         _hover={{color: "white"}} 
-         bg={"transparent"}>
-          Post
-         </Button>
-         )}
-       </InputRightElement>
-      </InputGroup>
-    </Flex>
-    </Box>
+        {!isProfilePage && (
+          <>
+            <Text fontSize={"sm"} fontWeight={700}>
+              {username}
+              {"   "}
+              <Text as="span" fontWeight={400}>
+                We just getting started
+              </Text>
+            </Text>
+            <Text fontSize={"small"} color={"grey"} cursor={"pointer"}>
+              View all 1,500 comments
+            </Text>
+          </>
+        )}
+        <Flex
+          alignItems={"center"}
+          gap={2}
+          justifyContent={"space-between"}
+          w={"full"}
+        >
+          {authUser && (
+            <InputGroup>
+              <Input
+                variant={"flushed"}
+                placeholder={"Add a comment..."}
+                fontSize={14}
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                ref={commentRef}
+              />
+              <InputRightElement>
+                {comment && (
+                  <Button
+                    fontSize={14}
+                    color={"grey"}
+                    fontWeight={600}
+                    cursor={"pointer"}
+                    _hidden={true}
+                    _hover={{ color: "white" }}
+                    bg={"transparent"}
+                    onClick={handleSubmitComment}
+                    isLoading={isCommenting}
+                  >
+                    Post
+                  </Button>
+                )}
+              </InputRightElement>
+            </InputGroup>
+          )}
+        </Flex>
+      </Box>
     </>
-  )
-}
-export default PostFooter
+  );
+};
+export default PostFooter;
---
+++ src/hooks/useLikePosts.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import useAuthStore from "../store/authStore";
+import useShowToast from "./useShowToast";
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
+
+const useLikePosts = (post) => {
+  const [isUpdating, setIsUpdating] = useState(false);
+  const authUser = useAuthStore((state) => state.user);
+  const showToast = useShowToast();
+
+  const [likes, setLikes] = useState(post?.likes?.length || 0);
+  const [isLiked, setIsLiked] = useState(
+    post?.likes?.includes(authUser?.uid) || false
+  );
+
+  useEffect(() => {
+    if (post && authUser) {
+      setLikes(post.likes.length);
+      setIsLiked(post.likes.includes(authUser.uid));
+    }
+  }, [post, authUser]);
+
+  const handleLikePost = async () => {
+    if (isUpdating) return;
+    if (!authUser) {
+      return showToast(
+        "Error",
+        "You must be logged in to like a post",
+        "error"
+      );
+    }
+
+    setIsUpdating(true);
+    try {
+      const postRef = doc(firestore, "posts", post.id);
+      await updateDoc(postRef, {
+        likes: isLiked ? arrayRemove(authUser.uid) : arrayUnion(authUser.uid),
+      });
+
+      setIsLiked(!isLiked);
+      setLikes((prevLikes) => (isLiked ? prevLikes - 1 : prevLikes + 1));
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  return { isLiked, likes, handleLikePost, isUpdating };
+};
+
+export default useLikePosts;
